1e631f5d72902e0b074bf0ad1bded38631e6d7be
vail0 <97312541+vail0@users.noreply.github.com>
2022-09-23 11:03:08 +0300

    views.py added groups_posts

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,9 @@
+from http.client import HTTPResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return HTTPResponse('Главная страница')
+
+def group_posts(request):
+    return HTTPResponse('Список сообществ')
