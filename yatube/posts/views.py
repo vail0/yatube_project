9b47f5a05a8f3520b7523d4999ba96211223940f
vail0 <97312541+vail0@users.noreply.github.com>
2022-09-23 11:47:14 +0300

    return slug

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,5 +5,5 @@ from django.shortcuts import render
 def index(request):
     return HttpResponse('Главная страница')
 
-def group_posts(request):
+def group_posts(request, slug):
     return HttpResponse('Список сообществ')
