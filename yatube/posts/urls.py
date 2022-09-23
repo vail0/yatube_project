1cf313773a04571e7aff9dac91028b5351b6f673
vail0 <97312541+vail0@users.noreply.github.com>
2022-09-23 11:01:22 +0300

    urls.py addedpath slug:slug

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', include(views.index)),
+    path('', views.index),
+    path('group/<slug:slug>/', views.group_posts),
 ]
