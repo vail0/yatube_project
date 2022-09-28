060e2325c94625f264129a5f7214b21a4e7d3562
vail0 <97312541+vail0@users.noreply.github.com>
2022-09-28 20:45:35 +0300

    added context

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,4 @@
+from turtle import title
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -5,10 +6,18 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     template = 'posts/index.html'
-    return render(request, template)
+    title = 'Это главная странциа проекта Yatube'
+    context = {
+               'title': title,
+    }
+    return render(request, template, context)
 #    return HttpResponse('Главная страница')
 
 def group_posts(request, slug):
     template = 'posts/group_list.html'
-    return render(request, template)
+    title = 'Здесь будет храниться информация о группах проекта yatube'
+    context = {
+               'title': title,
+    }
+    return render(request, template, context)
 #    return HttpResponse(f'Список сообществ {slug}')
