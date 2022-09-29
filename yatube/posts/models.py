7285969fbab76b8138747253463498ac131d3d4b
vail0 <97312541+vail0@users.noreply.github.com>
2022-09-29 14:46:25 +0300

    user added

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,3 +1,17 @@
+from cgitb import text
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.forms import DateTimeField
 
 # Create your models here.
+
+User = get_user_model()
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name = 'posts'
+    )
